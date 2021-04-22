6142036dd806f1016fce874a7cdab23a0254ce13
MarkCohn <markj@markcohn.com>
2021-04-22 14:47:39 +0000

    add runtime project

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -39,7 +39,7 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'gray');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight+100,'gray');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
